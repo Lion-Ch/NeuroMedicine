213ccc3ba6820772c6f6c90ebb7fc9a59e1ea726
Lev Chuprin <leva.chuprin@mail.ru>
2021-03-15 01:05:21 +0300

    Version 0.1

--- NeuroMedicine.BusinessLayer/ViewModels/DiagnosticVM.cs
+++ NeuroMedicine.BusinessLayer/ViewModels/DiagnosticVM.cs
@@ -5,12 +5,15 @@ using BusinessLayer.Logic.Extensions;
 using DataLayer.Models.Enums;
 using System.Windows.Input;
 using BusinessLayer.Commands;
+using System;
+using Common.NotifyPropertyChanged;
 
 namespace NeuroMedicine.BusinessLayer.ViewModels
 {
     public class DiagnosticVM : BaseViewModel
     {
         #region Поля класса
+
         private int _currentPage;
         public int CurrentPage
         {
@@ -21,6 +24,7 @@ namespace NeuroMedicine.BusinessLayer.ViewModels
                 SendPropertyChanged(() => CurrentPage);
             }
         }
+
         private bool[] _isEndabledPages;
         public bool[] IsEndabledPages
         {
@@ -31,8 +35,11 @@ namespace NeuroMedicine.BusinessLayer.ViewModels
                 SendPropertyChanged(() => IsEndabledPages);
             }
         }
+
         #endregion
+
         #region Поля изменения
+
         private ObservableCollection<ListItem> _diagnosticTypes;
         public ObservableCollection<ListItem> DiagnosticTypes
         {
@@ -50,20 +57,71 @@ namespace NeuroMedicine.BusinessLayer.ViewModels
             get { return _selectedDiagnosticType; }
             set { _selectedDiagnosticType = value; }
         }
+
         #endregion
+
         #region Команды
+
         private DelegateCommand _proceedCommand;
         public ICommand ProceedCommand { get { return _proceedCommand; } }
+        private DelegateCommand _loadPhotosCommand;
+        public ICommand LoadPhotosCommand { get { return _loadPhotosCommand; } }
+
+        private DelegateCommand _startAnalisisCommand;
+        public ICommand StartAnalisisCommand { get { return _startAnalisisCommand; } }
+
+        private DelegateCommand _addPatientCommand;
+        public ICommand AddPatientCommand { get { return _addPatientCommand; } }
+
+        private DelegateCommand _clearPatientListCommand;
+        public ICommand ClearPatientListCommand { get { return _clearPatientListCommand; } }
+
+        private DelegateCommand _registerNewPatientCommand;
+        public ICommand RegisterNewPatientCommand { get { return _registerNewPatientCommand; } }
+
         #endregion
+
         #region Методы команд
+
         private void Proceed(object parameter)
         {
             NextPage();
         }
+
+        private void StartAnalisis(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddPatient(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegisterNewPatient(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ClearPatientList(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadPhotos(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
+
         #region Публичные методы
+
+
         #endregion
+
         #region Приватные методы
+
         private void NextPage()
         {
             CurrentPage++;
@@ -80,6 +138,7 @@ namespace NeuroMedicine.BusinessLayer.ViewModels
                     break;
             }
         }
+
         #endregion
 
         public DiagnosticVM()
@@ -87,6 +146,11 @@ namespace NeuroMedicine.BusinessLayer.ViewModels
             HeaderVM = "Диагностика пациентов";
             DiagnosticTypes = new ListFactory().GetDiagnosticTypes().ToObservable();
             _proceedCommand = new DelegateCommand(this.Proceed);
+            _loadPhotosCommand = new DelegateCommand(this.LoadPhotos);
+            _clearPatientListCommand = new DelegateCommand(this.ClearPatientList);
+            _registerNewPatientCommand = new DelegateCommand(this.RegisterNewPatient);
+            _addPatientCommand = new DelegateCommand(this.AddPatient);
+            _startAnalisisCommand = new DelegateCommand(this.StartAnalisis);
             _currentPage = 0;
             _isEndabledPages = new bool[3] { true, false, false };
         }
